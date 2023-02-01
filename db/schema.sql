0921291e6c0e0edd5d1588bd51d01b965c37facb
Nya Tyus <nyastyus@gmail.com>
2023-02-01 14:24:51 -0500

    add starter sql files

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+-- no classes 
+-- like challenge 10 but with databases
+-- view, then add, then update
+-- select all from, insert into
+-- bonus is all joins
+
+-- 1. create database and seed data
+-- 2. set up 1 inquire message to view all employees
+-- after this, you can do everything after because it's repetition
+-- promise wrapper is the biggest headache
