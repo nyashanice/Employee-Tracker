add4f1a81b9f99c9a27b35a1e3b1d7313fe4143a
Nya Tyus <nyastyus@gmail.com>
2023-02-03 20:58:43 -0500

    use joins to show employee information in console

--- db/queries.sql
+++ db/queries.sql
@@ -10,3 +10,12 @@ SELECT e.first_name, e.last_name, CONCAT(m.first_name, " ", m.last_name) AS mana
 
 -- show roles
 SELECT r.id, r.title, r.salary, d.name AS department FROM role r LEFT JOIN department d ON r.department_id = d.id;
+
+-- show employees
+-- id, first, last, title, department, salary, manager
+SELECT e.id, e.first_name, e.last_name, r.title AS title, d.name AS department, r.salary, CONCAT(m.first_name, ' ',m.last_name) AS manager
+FROM employee e LEFT JOIN role r ON e.role_id = r.id
+JOIN department d ON r.department_id = d.id
+JOIN employee m ON e.manager_id = m.id;
+
+SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ',m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id JOIN role r ON e.role_id = r.id JOIN department d ON r.department_id = d.id;
--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function chooseOption() {
 }
 
 const viewEmployees = () => {
-    const sql = `SELECT * FROM employee`;
+    const sql = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ',m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id JOIN role r ON e.role_id = r.id JOIN department d ON r.department_id = d.id;`;
     db.query(sql, (err, result) => {
         if (err) {
             return console.error(error.message);
